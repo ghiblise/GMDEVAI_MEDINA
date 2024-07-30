a548dabe2fcda16960cd21367f0c18c9a3dc8540
unknown <astridilyse.medina@benilde.edu.ph>
2024-07-30 21:35:38 +0800

    Milestone 3: A More Detailed Graph

---
+++ Module 3/Assets/Scripts/FollowPath.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    Transform goal;
+    float speed = 5.0f;
+    float accuracy = 1.0f;
+    float rotSpeed = 2.0f;
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWaypointIndex = 0;
+    Graph graph;
+
+    void Start()
+    {
+        wps = wpManager.GetComponent<WaypointManager>().waypoints;
+        graph = wpManager.GetComponent<WaypointManager>().graph;
+        currentNode = wps[3];
+    }
+
+    void LateUpdate()
+    {
+        if (graph.getPathLength() == 0 || currentWaypointIndex == graph.getPathLength())
+        {
+            return;
+        }
+
+        currentNode = graph.getPathPoint(currentWaypointIndex);
+
+        if (Vector3.Distance(graph.getPathPoint(currentWaypointIndex).transform.position, transform.position) < accuracy)
+        {
+            currentWaypointIndex++;
+        }
+
+        if (currentWaypointIndex < graph.getPathLength())
+        {
+            goal = graph.getPathPoint(currentWaypointIndex).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+    }
+
+    public void GoToHelipad()
+    {
+        graph.AStar(currentNode, wps[17]);
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToRuins()
+    {
+        graph.AStar(currentNode, wps[21]);
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToFactory()
+    {
+        graph.AStar(currentNode, wps[15]);
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToTwinMountains()
+    {
+        graph.AStar(currentNode, wps[19]);
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToBarracks()
+    {
+        graph.AStar(currentNode, wps[20]);
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToCommandCenter()
+    {
+        graph.AStar(currentNode, wps[1]);
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToOilPumps()
+    {
+        graph.AStar(currentNode, wps[6]);
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToTankers()
+    {
+        graph.AStar(currentNode, wps[14]);
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToRadar()
+    {
+        graph.AStar(currentNode, wps[13]);
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToCommandPost()
+    {
+        graph.AStar(currentNode, wps[2]);
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToMiddle()
+    {
+        graph.AStar(currentNode, wps[4]);
+        currentWaypointIndex = 0;
+    }
+}
